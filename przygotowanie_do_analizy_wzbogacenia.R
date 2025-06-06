40dc36e45635f511d4ace807ada087555cce19a5
olleskiew <147051180+olleskiew@users.noreply.github.com>
2025-06-06 15:11:30 +0200

    Gotowe przekształcanie DAVID.txt
    Gotowy kod, przekształcający DAVID.txt do użytecznego pliku .csv

--- przygotowanie_do_analizy_wzbogacenia.R
+++ przygotowanie_do_analizy_wzbogacenia.R
@@ -1,7 +1,10 @@
 library(tidyverse)
-#Test działania na jednym pliku, zastąp później 0 0:8
-file_list <- paste0("DAVID_wyniki/DAVID_klaster_", 0, ".txt")
+#Przygotowanie listy sciezek do wczytywania plików
+file_list <- paste0("DAVID_wyniki/DAVID_klaster_", 0:8, ".txt")
 print(file_list)
+
+#Ramka danych, w której będziemy przetrzymywać zbiorcze wyniki, z wszystkich plików
+df_all <- tibble()
 
 for( sciezka in file_list ){
   file <- read_lines(sciezka)
@@ -36,24 +39,45 @@ for( sciezka in file_list ){
     
     parsed<-str_split_fixed(GO_lines, "\\t", n=13)
     
+    #Nadajmy nazwę kolumnom
     colnames(parsed) <- c(
       "Category", "Term", "Count", "Percent", "PValue", "Genes",
       "List_Total", "Pop_Hits", "Pop_Total", "Fold_Enrichment",
       "Bonferroni", "Benjamini", "FDR")
       
+    #df_tymczas przechowuje informacje o jednym klastrze z DAVIDowego pliku na raz
     df_tymczas <- as_tibble(parsed) %>% 
       mutate(
         Cluster = cluster_number,
         Enrichment_score = enrichment_score
       ) %>% 
-      separate(col = "Term", into = c("GO_ID", "GO_term"), sep="~") %>% 
-      select(Cluster, Enrichment_score, everything())
+      separate(col = "Term", into = c("GO_ID", "GO_term"), sep="~") %>% #Rozdzielnie Term na dwie kolumny
+      select(Cluster, Enrichment_score, everything()) %>% #Ustawiam ramkę danych tak, by Cluster i Enrichment_score były na początku
+      mutate(across(-c(Category, GO_ID, GO_term, Genes), as.numeric)) #Zmiana typu wszystkich kolumn na numeric poza Category, GO_ID, GO_term i Genes
+             
     
+    #sklejenie wszystkich klastrow z danego pliku
     df_out <-bind_rows(df_out, df_tymczas)
-    headers <- colnames(df_out)
-    print(headers)
+    
+    #Wciąganie numeru pliku za ścieżki
+    plik_id <- str_extract(sciezka, "\\d+")
+    
+    #Dopisanie kolumny plik z informacją o numerze pliku, z której pochodzi lista genów
+    df_out<- df_out %>% 
+      mutate(Plik=plik_id)
+    
+    #Zapis pliku csv dla każdej listy genów osobno
+    write_csv(df_out, paste0("DAVID_wyniki/parsed_klaster_", plik_id, ".csv"))
+    
+    #Dopisanie ramki danych dla listy do pliku zbiorczego
+    df_all <- bind_rows(df_all, df_out)
+  
     
   }
-  glimpse(df_out)
-  View(df_out)
+  
 }
+
+#Zapis pliku zbiorczego
+write.csv(df_all, "DAVID_wyniki/parsed_all_clusters.csv")
+komunikat<-"Zakończono działanie, wszystkie wyniki zostały zapisane"
+print(komunikat)
