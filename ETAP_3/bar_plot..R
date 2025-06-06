468afe8cef60d046c9fa3653e2478b6ef11f0be2
olleskiew <147051180+olleskiew@users.noreply.github.com>
2025-06-07 00:07:03 +0200

    Kod gotowy
    Kod jest gotowy ale muszę poszukac jeszcze literówek powstałych na skutek ręcznej annotacji

--- ETAP_3/bar_plot..R
+++ ETAP_3/bar_plot..R
@@ -1,15 +1,32 @@
 #Skrypt do stworzenia barplotu:
 library(tidyverse)
 
-
+#Wczytanie danych - pliku DAVID.csv i pliku z annotacjami
 dane <- read.csv("parsed_all_clusters.csv")
 annotations <- read.csv("plik_z_anntocją.csv")
+
+#Zdefiniowanie funkcji do ujednolicenia zapisu annotacji
+capitalize_first_word <- function(x)
+{
+  str_replace(x, "^(\\w)(\\w*)", function(m) 
+    {
+    paste0(toupper(substr(m, 1, 1)), substr(m, 2, nchar(m)))
+  })
+}
 
 GO_data_processed <- dane %>% 
   filter(Enrichment_score >=1) %>% 
   select(Plik, Cluster, GO_term, Count) %>% 
   left_join(annotations %>% select(Plik, Cluster, Biological_process),
             by = c("Plik", "Cluster"))
+
+
+GO_data_processed <- GO_data_processed %>% 
+  mutate(Biological_process = capitalize_first_word(Biological_process)) %>% 
+  mutate(Biological_process = recode(Biological_process,
+                                     "Biosynteza Hemu" = "Biosynteza hemu",
+                                     "biosynteza hemu" = "Biosynteza hemu",
+                                     "Biosytneza hemu" = "Biosynteza hemu"))
 
 
 summed <- GO_data_processed %>% 
@@ -22,14 +39,26 @@ summed <- GO_data_processed %>%
   ) %>% 
   ungroup()
 
-View(GO_data_processed)
+colors <- c(
+  "G-CSF" = "#CD5733",         # intensywny pomarańcz
+  "klastry wspólne" = "#F4E7C5FF",  # granatowo-szary
+  "Kontrola" = "#678096"       # głęboka czerwień
+)
+
 
 ggplot(summed, aes(x=Suma, y=Biological_process, fill=Condition))+
   geom_bar(stat = "identity")+
+  scale_fill_manual(values = colors)+
   labs(
     title = "Analiza wzbogacenia",
     x = "Suma zliczeń",
-    y = "Proces biologiczny"
+    y = "Proces biologiczny",
+    fill = "Rodzaj klastra"
   )+
-  theme_minimal()
+  theme_minimal(base_size = 14)+
+  theme(
+    axis.text.y = element_text(size =10, margin = margin(r=10))
+  )
+
+ggsave("barplot_procesy_biologiczne.png", width =12, height = 10, dpi =300)
 #sessionInfo()
