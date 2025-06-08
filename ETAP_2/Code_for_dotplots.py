a490c841a93f62b7d16eaf53b79fdbe01102785a
olleskiew <147051180+olleskiew@users.noreply.github.com>
2025-06-08 14:11:45 +0200

    v.1.2
    Adding new version of code

--- ETAP_2/Code_for_dotplots.py
+++ ETAP_2/Code_for_dotplots.py
@@ -1,5 +1,6 @@
 import scanpy as sc
 import pandas as pd
+import numpy as np
 
 def main():
     print("Rozpoczynam pracę")
@@ -9,16 +10,19 @@ def main():
     genes = pd.read_csv("gene_list.csv")["names"].tolist()
     print("Lista genów została wczytana")
 
+    sc.settings.set_figure_params(format='svg')
+
     sc.pl.dotplot(
         adata,
         var_names=genes,
         groupby="louvain",
-        #use_raw=True,
-        #log=True,
-        #standard_scale="var",
+        group_order=["4", "7", "8", "5","6", "0", "1", "2", "3"],
+        use_raw=True,
+        log=True,
+        standard_scale="var",
         figsize=(10, 6),
         show=False,
-        save="dotplot_new_2.png",
+        save="dotplot_new_3",
     )
 
     print("Stworzono dotplot")
